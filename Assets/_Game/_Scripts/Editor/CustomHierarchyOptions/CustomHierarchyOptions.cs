adb8d11e4cff5689828a335cc0800df25c9a2eee
Matheus Duca <theu.duka@gmail.com>
2025-12-01 16:30:47 -0300

    feat: toggle gameObject.SetActive

--- Assets/_Game/_Scripts/Editor/CustomHierarchyOptions/CustomHierarchyOptions.cs
+++ Assets/_Game/_Scripts/Editor/CustomHierarchyOptions/CustomHierarchyOptions.cs
@@ -11,9 +11,34 @@ namespace EditorToolsPractice
             EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;            
         }
 
-        private static void HierarchyWindowItemOnGUI(int idValue, Rect rectValue)
+        private static void HierarchyWindowItemOnGUI(int id, Rect rect)
         {
-            Debug.Log("Script foi chamado!");            
+            RenderButtonWithToggle(id, rect);
+        }
+
+        private static Rect DrawRect(float x, float y, float width, float height)
+        {
+            return new Rect(x, y, width, height);
+        }
+
+        private static void DrawButtonWithToggle(int id, float x, float y, float width, float height)
+        {
+            GameObject gameObject = EditorUtility.InstanceIDToObject(id) as GameObject;
+
+            if (gameObject == null) return;
+
+            Rect toggleRect = DrawRect(x, y, width, height);
+            gameObject.SetActive(GUI.Toggle(toggleRect, gameObject.activeSelf, string.Empty));
+        }
+
+        private static void RenderButtonWithToggle(int id, Rect rect)
+        {
+            float offsetX = 20;
+            float offsetY = -3;
+            float width = 10;
+            float height = 10;
+
+            DrawButtonWithToggle(id, rect.x - offsetX, rect.y - offsetY, rect.width, rect.height);
         }
     }
 }
