990cc8e8f69e0736398fad4a54a2694a4e85bbb4
Matheus Duca <theu.duka@gmail.com>
2025-12-01 17:00:04 -0300

    feat: criação de uma classe de Utilidades para HierarchyGUI

--- Assets/_Game/_Scripts/Editor/CustomHierarchyOptions/ToggleGameObjectTool.cs
+++ Assets/_Game/_Scripts/Editor/CustomHierarchyOptions/ToggleGameObjectTool.cs
@@ -9,30 +9,9 @@ namespace EditorToolsPractice
     [InitializeOnLoad]
     public class ToggleGameObjectTool : Editor
     {
-        static ToggleGameObjectTool()
-        {
-            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;            
-        }
+        static ToggleGameObjectTool() => EditorApplication.hierarchyWindowItemOnGUI += AttachToggleItemOnGUI;
 
-        private static void HierarchyWindowItemOnGUI(int id, Rect rect)
-        {
-            RenderButtonWithToggle(id, rect);
-        }
-
-        private static Rect DrawRect(float x, float y, float width, float height)
-        {
-            return new Rect(x, y, width, height);
-        }
-
-        private static void DrawButtonWithToggle(int id, float x, float y, float width, float height)
-        {
-            GameObject gameObject = EditorUtility.InstanceIDToObject(id) as GameObject;
-
-            if (gameObject == null) return;
-
-            Rect toggleRect = DrawRect(x, y, width, height);
-            gameObject.SetActive(GUI.Toggle(toggleRect, gameObject.activeSelf, string.Empty));
-        }
+        private static void AttachToggleItemOnGUI(int id, Rect rect) => RenderButtonWithToggle(id, rect);
 
         private static void RenderButtonWithToggle(int id, Rect rect)
         {
@@ -41,7 +20,7 @@ namespace EditorToolsPractice
             float width = 10;
             float height = 10;
 
-            DrawButtonWithToggle(id, rect.x - offsetX, rect.y - offsetY, width, height);
+            HierarchyGUIUtils.DrawButtonWithToggle(id, rect.x - offsetX, rect.y - offsetY, width, height);
         }
     }
 }
